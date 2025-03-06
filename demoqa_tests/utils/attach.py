b9c664cc02b901df44f4284efb377ce0f86c969e
Rostislav Ivanov <x1qeee25@gmail.com>
2025-03-06 08:58:00 +0300

    Правки

--- demoqa_tests/utils/attach.py
+++ demoqa_tests/utils/attach.py
@@ -22,5 +22,4 @@ def add_video(browser):
     html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
            + video_url \
            + "' type='video/mp4'></video></body></html>"
-    allure.attach(body=html, name='video_' + browser.driver.session_id, attachment_type=AttachmentType.HTML,
-        extension='.html')
+    allure.attach(html, 'video_' + browser.driver.session_id, AttachmentType.HTML, '.html')
