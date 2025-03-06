04e89c25759eef6c8ba2d2ea334ffefc72c5aae8
Rostislav Ivanov <x1qeee25@gmail.com>
2025-03-06 09:00:29 +0300

    Правки

--- demoqa_tests/utils/attach.py
+++ demoqa_tests/utils/attach.py
@@ -9,12 +9,12 @@ def add_screenshot(browser):
 
 def add_logs(browser):
     log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
-    allure.attach(body=log, name='browser_logs', attachment_type=AttachmentType.TEXT, extension='.log')
+    allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
 
 
 def add_html(browser):
     html = browser.driver.page_source
-    allure.attach(body=html, name='page_source', attachment_type=AttachmentType.HTML, extension='.html')
+    allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
 
 
 def add_video(browser):
